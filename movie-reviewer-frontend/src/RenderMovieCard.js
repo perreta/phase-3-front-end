d9bcc861703db855defdf35e72cffff508e189b1
cpan1995 <cpan1995@gmail.com>
2021-08-25 14:34:16 -0400

    added filtering

--- movie-reviewer-frontend/src/RenderMovieCard.js
+++ movie-reviewer-frontend/src/RenderMovieCard.js
@@ -1,12 +1,30 @@
-import { useEffect, useState } from "react";
-import { Card, Popup, Image } from "semantic-ui-react";
+import { useEffect, useState, useHistory } from "react";
+import { Card, Popup, Image, Button } from "semantic-ui-react";
 
-export default function RenderMovieCard({title, imdbId, posterImg, year}){
+export default function RenderMovieCard({title, imdbId, posterImg, year, plot}){
+
+    // let history = useHistory()
+    
+    // function handleClick(){
+    //     history.push()
+    // }
+    
     return(
         <Popup trigger = {
             <Card>
                 <Image src={posterImg} wrapped ui={false}/>
                 <Card.Header>{`${title} (${year})`}</Card.Header>
+                <Card.Description className = 'plotDescriptions'>
+                    {plot}
+                </Card.Description>
+                
+                    <div>
+                        <Button fluid animated='fade'>
+                            <Button.Content visible>Reviews</Button.Content>
+                            <Button.Content hidden>Click to See or Add Reviews</Button.Content>
+                        </Button>
+                    </div>
+                
             </Card>
         }>
         </Popup>
--- movie-reviewer-frontend/src/RenderSearchResults.js
+++ movie-reviewer-frontend/src/RenderSearchResults.js
@@ -3,6 +3,26 @@ import RenderMovieCard from "./RenderMovieCard";
 import { v4 as uuidv4 } from 'uuid';
 import { Card } from 'semantic-ui-react' 
 
+
+function filterMovies(movie, selectedObject){
+    if(movie.Poster !== 'N/A' || !movie.Plot !== 'N/A' || movie.Title !== 'N/A'){
+        if(selectedObject.yearStart <= parseInt(movie.Year) && selectedObject.yearEnd >= parseInt(movie.Year)){
+            console.log("filtering")
+            if(selectedObject.genreOptions.length == 0){
+                return true
+            }
+            else{
+                let genreChecker=movie.Genre.replace('-','').toLowerCase().split(', ')
+                console.log(genreChecker)
+                return genreChecker.some(genre => selectedObject.genreOptions.includes(genre))
+            }
+        }
+        else{
+            return false;
+        }
+    }
+}
+
 export default function RenderSearchResults({searchObject}){
     const apiKey = "799cdd3c";
     const [movieSearch, setMoviesSearch] = useState([])
@@ -13,20 +33,29 @@ export default function RenderSearchResults({searchObject}){
             fetch(`https://www.omdbapi.com/?apikey=${apiKey}&s=${searchObject.searchTerm}&page=1`)
             .then(res => res.json())
             .then(data => {
-                setMoviesSearch(data.Search)
+                let storeMovies = []
+                const moviePromises = data.Search.map(movie => {
+                    return fetch(`https://www.omdbapi.com/?apikey=${apiKey}&i=${movie.imdbID}`)
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log("fetching data")
+                        storeMovies.push(data)
+                    })
+                })
+                Promise.all(moviePromises).then(() => {
+                    setMoviesSearch(storeMovies)
+                })
             })
         }
-        
     }, [searchObject]);
 
     if(movieSearch.length != 0){
         movieCards = movieSearch.map(movie => {
-            return <RenderMovieCard key={uuidv4()} title={movie.Title} imdbId={movie.imdbID} posterImg = {movie.Poster} year = {movie.Year}/>
+            if(filterMovies(movie, searchObject)){
+                return <RenderMovieCard key={uuidv4()} title={movie.Title} imdbId={movie.imdbID} posterImg = {movie.Poster} year = {movie.Year} plot={movie.Plot}/>
+            }
         })
-        console.log(movieCards)
     }
-
-    
 
     return(
         <Card.Group className='cardGroupSearch'>
