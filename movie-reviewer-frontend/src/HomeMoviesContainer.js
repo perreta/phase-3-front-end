51afe94b3ba73b254d73069271de83585f89ed24
Quinn Lonergan <qui99lo@gmail.com>
2021-08-26 12:31:13 -0400

    Three more carousels

--- movie-reviewer-frontend/src/HomeMoviesContainer.js
+++ movie-reviewer-frontend/src/HomeMoviesContainer.js
@@ -6,6 +6,9 @@ import ItemsCarousel from 'react-items-carousel';
 
 function HomeMoviesContainer() {
     const [homeMovies, setHomeMovies] = useState([]);
+    const [topRatedMovies, setTopRatedMovies] = useState([]);
+    const [oldies, setOldies] = useState([]);
+    const [animation, setAnimation] = useState([]);
   
     useEffect(() => {
       fetch("http://localhost:9292/")
@@ -13,10 +16,33 @@ function HomeMoviesContainer() {
         .then((homeMovies) => setHomeMovies(homeMovies));
     }, []);
 
+    useEffect(() => {
+      fetch("http://localhost:9292/toprated")
+        .then((r) => r.json())
+        .then((topRatedMovies) => setTopRatedMovies(topRatedMovies));
+    }, []);
+
+    useEffect(() => {
+      fetch("http://localhost:9292/oldies")
+        .then((r) => r.json())
+        .then((oldies) => setOldies(oldies));
+    }, []);
+
+    useEffect(() => {
+      fetch("http://localhost:9292/animation")
+        .then((r) => r.json())
+        .then((animation) => setAnimation(animation));
+    }, []);
+
     const [activeItemIndex, setActiveItemIndex] = useState(0);
+    const [activeItemIndex2, setActiveItemIndex2] = useState(0);
+    const [activeItemIndex3, setActiveItemIndex3] = useState(0);
+    const [activeItemIndex4, setActiveItemIndex4] = useState(0);
     const chevronWidth = 40;
 
   return (
+    <>
+    <h1>Popular Now</h1>
     <div style={{ padding: `0 ${chevronWidth}px` }}>
       <ItemsCarousel
         requestToChangeActive={setActiveItemIndex}
@@ -28,15 +54,87 @@ function HomeMoviesContainer() {
         outsideChevron
         chevronWidth={chevronWidth}
       >
-          {homeMovies.map((movie) => (
+          {homeMovies.map((movie) => {
+            if (movie.img_url !== "N/A" && movie.img_url)
+            return (
             <img
                 style ={{height: 400, width: 290}}
               src={movie.img_url}
               alt="Poster not found"
-            />
-          ))}
+            />)}
+          )}
       </ItemsCarousel>
     </div>
+    <h1>Top Rated Movies</h1>
+    <div style={{ padding: `0 ${chevronWidth}px` }}>
+      <ItemsCarousel
+        requestToChangeActive={setActiveItemIndex2}
+        activeItemIndex={activeItemIndex2}
+        numberOfCards={4}
+        gutter={20}
+        leftChevron={<button>{'<'}</button>}
+        rightChevron={<button>{'>'}</button>}
+        outsideChevron
+        chevronWidth={chevronWidth}
+      >
+          {topRatedMovies.map((movie) => {
+            if (movie.img_url !== "N/A" && movie.img_url)
+            return (
+            <img
+                style ={{height: 400, width: 290}}
+              src={movie.img_url}
+              alt="Poster not found"
+            />)}
+          )}
+      </ItemsCarousel>
+    </div>
+    <h1>Oldies</h1>
+    <div style={{ padding: `0 ${chevronWidth}px` }}>
+      <ItemsCarousel
+        requestToChangeActive={setActiveItemIndex3}
+        activeItemIndex={activeItemIndex3}
+        numberOfCards={4}
+        gutter={20}
+        leftChevron={<button>{'<'}</button>}
+        rightChevron={<button>{'>'}</button>}
+        outsideChevron
+        chevronWidth={chevronWidth}
+      >
+          {oldies.map((movie) => {
+            if (movie.img_url !== "N/A" && movie.img_url)
+            return (
+            <img
+                style ={{height: 400, width: 290}}
+              src={movie.img_url}
+              alt="Poster not found"
+            />)}
+          )}
+      </ItemsCarousel>
+    </div>
+    <h1>Animation</h1>
+    <div style={{ padding: `0 ${chevronWidth}px` }}>
+      <ItemsCarousel
+        requestToChangeActive={setActiveItemIndex4}
+        activeItemIndex={activeItemIndex4}
+        numberOfCards={4}
+        gutter={20}
+        leftChevron={<button>{'<'}</button>}
+        rightChevron={<button>{'>'}</button>}
+        outsideChevron
+        chevronWidth={chevronWidth}
+      >
+          {animation.map((movie) => {
+            if (movie.img_url !== "N/A" && movie.img_url)
+            return (
+            <img
+                style ={{height: 400, width: 290}}
+              src={movie.img_url}
+              alt="Poster not found"
+            />)}
+          )}
+      </ItemsCarousel>
+    </div>
+    </>
   );
 
     //   return (
