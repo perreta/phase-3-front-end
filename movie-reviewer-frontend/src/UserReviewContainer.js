f5dd1efb05185d322fdd825aa4ffc75db137469e
Cheng Pan <61174986+cpan1995@users.noreply.github.com>
2021-08-26 14:47:37 -0400

    Create UserReviewContainer.js

---
+++ movie-reviewer-frontend/src/UserReviewContainer.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react'
+
+import UserReview from "./UserReview"
+
+function UserReviewContainer({ user }) {
+
+    const [ reviewArray, setReviewArray ] = useState([])
+    useEffect(() => {
+        fetch(`http://localhost:9292/users/${user.id}`)
+            .then(resp=>resp.json())
+            .then(data=>setReviewArray(data.reviews))
+    }, [])
+   
+    const review = reviewArray.map(review => {
+        return <UserReview key={review.id} title={review.title} poster={review.poster} rating={review.rating} content={review.review}/>
+    })
+
+    return (
+        <div className="reviewcontainer">
+            {review}
+        </div>
+    )
+}
+export default UserReviewContainer
