2b34849e0bee2e9f756884d71bc78cf7838a46cc
Cheng Pan <61174986+cpan1995@users.noreply.github.com>
2021-08-26 14:47:13 -0400

    Update UserReview.js

--- movie-reviewer-frontend/src/UserReview.js
+++ movie-reviewer-frontend/src/UserReview.js
@@ -1,1 +1,17 @@
+function UserReview({ title, poster, rating, content }) {
 
+  return(
+    <div className="container" >
+      <div className="poster">
+        <img src={poster} alt="Poster" style={{ maxWidth: "200px", width:"100%" }}/>
+      </div>
+      <div className="review">
+        <h1>{title}</h1>
+        <h3>Rating: {rating}/10</h3>
+        <p>Review: {content}</p>
+      </div>
+    </div>
+  )
+}
+  
+export default UserReview;
