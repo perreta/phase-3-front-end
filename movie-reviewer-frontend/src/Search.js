0925d2461654bece6b68711679caad01535daacb
cpan1995 <cpan1995@gmail.com>
2021-08-27 10:37:19 -0400

    final

--- movie-reviewer-frontend/src/Search.js
+++ movie-reviewer-frontend/src/Search.js
@@ -86,6 +86,7 @@ export default function Search(){
                 />
                 <Button type='submit' onClick={handleButtonSearch}>Search</Button>
             </form>
+            <br/>
             <RenderSearchResults searchObject = {searchObject}/>
             
         </div>
